770d9fac41417969d8fc9ba57caa40e9c2a92029
ZhaoPeichen <1981336562@qq.com>
2023-11-10 16:59:37 +0800

    rename route.ts to router.ts

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import store from './store'
-import router from './route'
+import router from './router'
 import './style.css'
 
 createApp(App)
