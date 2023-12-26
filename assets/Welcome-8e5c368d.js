eaf3a9f92ee87add0e832b4406178b07ad624ab3
Langingsing <Langingsing@users.noreply.github.com>
2023-12-26 14:25:22 +0000

    deploy: dd1aaa776ddc48b0acb5e57ebaf4309ef8401652

---
+++ assets/Welcome-8e5c368d.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-c190d87f.js";const c={};function r(t,_){return" 自主选择前端、后台开发框架，实现《好去处》原型系统部分功能模块，数 据库可选择开源的 MySQL 或者其他类型数据库。原型系统开发要求涉及至少一 种前端、两种后台开源框架技术（其中一种指数据持久化相关技术框架，如 Mybatis 等）的应用。 "}const o=e(c,[["render",r]]);export{o as default};
