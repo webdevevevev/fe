48c683e23d2f14a84c282b77352ae7e6b1dd4c0e
ZhaoPeichen <1981336562@qq.com>
2023-12-27 21:25:16 +0800

    remove keep alive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,7 @@
 </script>
 
 <template>
-    <router-view v-slot="{ Component }">
-        <keep-alive>
-            <component :is="Component"/>
-        </keep-alive>
-    </router-view>
+    <router-view/>
 </template>
 
 <style scoped>
